4ff94af0f55ebc9e06e43ad3695151be11f2a334
shivamkumarlnct <shivamsitu2@gmail.com>
2024-07-10 02:10:30 +0530

    crud

--- index.js
+++ index.js
@@ -16,16 +16,31 @@ const userSchema=new mongoose.Schema({
 })
 const User=mongoose.model('User',userSchema);
 
-//it is insert multiple 
-User.insertMany([
-  {name:"tony",email:"tony@gmail.com",age:50},
-  {name:"aman",email:"aman@gmail.com",age:20},
-  {name:"tanu",email:"tanu@gmail.com",age:30},
-]).then((res)=>{
-  console.log(res);
-})
+// -----------model find------------------
+// User.find({}).then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
 
-//insert only one data
+// ----------------------
+// User.findById("66885de91cf672919ef50129").then((res)=>{
+//   // console.log(res[0].name);
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+//it is insert multiple ----------------
+// User.insertMany([
+//   {name:"tony",email:"tony@gmail.com",age:50},
+//   {name:"aman",email:"aman@gmail.com",age:20},
+//   {name:"tanu",email:"tanu@gmail.com",age:30},
+// ]).then((res)=>{
+//   console.log(res);
+// })
+
+//insert only one data---------------------
 
 // const user1=new User({
 //   name:"shivam",
@@ -39,6 +54,70 @@ User.insertMany([
 // })
 // user1.save();
 // user2.save().then((res)=>{//it defind the err
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// ----update one------------
+// User.updateOne({name:"tanu"},{age:200}).then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+// ------update many---------
+// User.updateMany({age:{$gt:25}},{age:35}).then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+// ------------findandupdate------------
+// User.findOneAndUpdate({name:"tanu"},{age:22},{new:true}).then((res)=>{
+// console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// -------findbyid-----------------
+
+// User.findByIdAndUpdate("6689a42c5f272dbbbce1645b",{age:42},{new:true}).then((res)=>{
+// console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// ---------delete------------
+// User.deleteOne({name:"shivam"}).then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// ---------delete many--------------
+// User.deleteMany({age:35}).then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// ------findbyidanddelet------
+
+// User.findByIdAndDelete("6689a603856e184c9d32a51f").then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// ---findOneanddelete----------
+
+// User.findOneAndDelete({name:"aman"}).then((res)=>{
 //   console.log(res);
 // })
 // .catch((err)=>{
